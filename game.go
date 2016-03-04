2dc67cd2dfe7967f571290787a3a78b75d8a12e9
ManiacMike <mikewantacareer@gmail.com>
2016-03-04 18:33:04 +0800

    test gwork

--- game.go
+++ game.go
@@ -3,14 +3,150 @@ package main
 import (
 	"math/rand"
 	"time"
+	"github.com/user/gwork"
+	"sort"
 )
 
 const MAX_CARD_NUM = 450
 
 const WIN_SCORE = 30
 
+type DixitRoom struct {
+		RoomId	  string
+		host      string
+		round     int
+		score     map[string]int
+		cardStack []int
+		realcard  int
+		falsecard map[string]int
+		guess     map[string]int
+		keyword   string
+		gworkRoom *gwork.Room
+}
+
+var DixitRoomList map[string]DixitRoom
+
+func (room *DixitRoom) StartGame(creator string) {
+	gwork.MysqlQuery("UPDATE `game` SET status = 1 WHERE `id`= " + room.RoomId)
+	room.host = creator
+	room.score = make(map[string]int)
+	room.round = 1
+	room.realcard = 0
+	room.falsecard = make(map[string]int)
+	room.guess = make(map[string]int)
+	room.keyword = ""
+	var stack []int
+	for index := 1; index < MAX_CARD_NUM+1; index++ {
+		stack = append(stack, index)
+	}
+	room.cardStack = stack
+	//抽牌
+	for _, user := range room.gworkRoom.Userlist {
+		cards, cardStack := drawCards(6, room.cardStack)
+		room.cardStack = cardStack
+		//user.cards = cards
+		// room.Userlist[index] = user
+		replyBody := make(map[string]interface{})
+		replyBody["type"] = "start"
+		replyBody["cards"] = cards
+		replyBody["host"] = room.host
+		room.gworkRoom.Push(user, gwork.JsonEncode(replyBody))
+	}
+	DixitRoomList[room.RoomId] = *room
+}
+
+func (room *DixitRoom) HostPick(keyword string, card int) {
+	room.keyword = keyword
+	room.realcard = card
+	reply := make(map[string]string)
+	reply["type"] = "hostpick"
+	reply["keyword"] = keyword
+	replyBody := gwork.JsonEncode(reply)
+	room.gworkRoom.Broadcast(replyBody)
+	DixitRoomList[room.RoomId] = *room
+}
+
+func (room *DixitRoom) GuestPick(uid string, card int) {
+	room.falsecard[uid] = card
+	flag := true
+	for _, u := range room.gworkRoom.Userlist {
+		if u.Uid != room.host {
+			_, exist := room.falsecard[u.Uid]
+			if exist == false {
+				flag = false
+			}
+		}
+	}
+	if flag == true {
+		cards := []int{}
+		for _, c := range room.falsecard {
+			cards = append(cards, c)
+		}
+		cards = append(cards, room.realcard)
+		sort.Ints(cards)
+		replyBody := make(map[string]interface{})
+		replyBody["type"] = "showcards"
+		replyBody["cards"] = cards
+		room.gworkRoom.Broadcast(gwork.JsonEncode(replyBody))
+	}
+	DixitRoomList[room.RoomId] = *room
+}
+
+func (room *DixitRoom) Guess(uid string, card int) {
+	room.guess[uid] = card
+	flag := true
+	for _, u := range room.gworkRoom.Userlist {
+		if u.Uid != room.host {
+			_, exist := room.guess[u.Uid]
+			if exist == false {
+				flag = false
+			}
+		}
+	}
+	if flag == true {
+		score, gameover := gameResult(room)
+		room.score = score
+		cards := room.falsecard
+		cards[room.host] = room.realcard
+		replyBody := make(map[string]interface{})
+		replyBody["type"] = "result"
+		replyBody["gameover"] = gameover
+		replyBody["cards"] = cards
+		replyBody["score"] = score
+		if gameover == false {
+			fillCards := room.roundInit()
+			replyBody["round"] = room.round
+			replyBody["host"] = room.host
+			for _, u := range room.gworkRoom.Userlist {
+				replyBody["fillcard"] = fillCards[u.Uid]
+				room.gworkRoom.Push(u, gwork.JsonEncode(replyBody))
+			}
+		} else {
+			room.gworkRoom.Broadcast(gwork.JsonEncode(replyBody))
+		}
+	}
+	DixitRoomList[room.RoomId] = *room
+}
+
+func (room *DixitRoom) roundInit() map[string]int {
+	fillCards := make(map[string]int)
+	for _, u := range room.gworkRoom.Userlist {
+		cards, cardStack := drawCards(1, room.cardStack)
+		room.cardStack = cardStack
+		fillCards[u.Uid] = cards[0]
+	}
+	room.realcard = 0
+	room.falsecard = make(map[string]int)
+	room.guess = make(map[string]int)
+	room.keyword = ""
+	room.round++
+	room.host = nextHost(room.host, room.gworkRoom.Userlist)
+	DixitRoomList[room.RoomId] = *room
+	return fillCards
+}
+
 //判定分数 补牌 开始下一轮
-func gameResult(room *Room) (map[string]int, bool) {
+func gameResult(room *DixitRoom) (map[string]int, bool) {
 	score := make(map[string]int)
 	revertGuess := make(map[int]string)
 	var wrongCardUid string
@@ -35,9 +171,9 @@ func gameResult(room *Room) (map[string]int, bool) {
 		score[room.host] = 0
 	}
 	totalScore := room.score
-	for _, u := range room.userlist {
-		totalScore[u.uid] = totalScore[u.uid] + score[u.uid]
-		if totalScore[u.uid] > WIN_SCORE || totalScore[u.uid] == WIN_SCORE {
+	for _, u := range room.gworkRoom.Userlist {
+		totalScore[u.Uid] = totalScore[u.Uid] + score[u.Uid]
+		if totalScore[u.Uid] > WIN_SCORE || totalScore[u.Uid] == WIN_SCORE {
 			gameover = true
 		}
 	}
@@ -62,11 +198,11 @@ func drawCards(num int, stack []int) ([]int, []int) {
 	return getCards, stack
 }
 
-func nextHost(curHost string, ulist UserList) string {
+func nextHost(curHost string, ulist gwork.UserList) string {
 	var index, next int
 	var flag = false
 	for i, u := range ulist {
-		if curHost == u.uid {
+		if curHost == u.Uid {
 			index = i
 			flag = true
 			break
@@ -82,5 +218,5 @@ func nextHost(curHost string, ulist UserList) string {
 	} else {
 		next = 0
 	}
-	return ulist[next].uid
+	return ulist[next].Uid
 }
--- main.go
+++ main.go
@@ -1,70 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"github.com/larspensjo/config"
-	"golang.org/x/net/websocket"
-	"log"
-	"net/http"
-	// "time"
+	"github.com/user/gwork"
 )
 
-var roomList map[string]Room //在线room列表
-
-type ServiceError struct {
-	Msg string
-}
-
-func (e *ServiceError) Error() string {
-	return fmt.Sprintf("%s", e.Msg)
-}
-
-func Error(msg string) error {
-	return &ServiceError{msg}
-}
-
-func CheckErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	roomList = make(map[string]Room)
-	http.Handle("/", websocket.Handler(WsServer))
-	serverConfig, err := getConfig("server")
-	if err != nil {
-		log.Fatal("server config error:", err)
-	}
-	fmt.Println("listen on port " + serverConfig["port"])
-
-	if err := http.ListenAndServe(":"+serverConfig["port"], nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
-}
-
-func getConfig(sec string) (map[string]string, error) {
-	targetConfig := make(map[string]string)
-	cfg, err := config.ReadDefault("config.ini")
-	if err != nil {
-		return targetConfig, Error("unable to open config file or wrong fomart")
-	}
-	sections := cfg.Sections()
-	if len(sections) == 0 {
-		return targetConfig, Error("no " + sec + " config")
-	}
-	for _, section := range sections {
-		if section != sec {
-			continue
+	gwork.Init(func(receiveNodes map[string]interface{}, uid string, room *gwork.Room){
+		receiveType := receiveNodes["type"]
+		rid := room.RoomId
+		DixitRoomList = make(map[string]DixitRoom)
+		gameRoom,ok := DixitRoomList[rid]
+		if ok == false{
+			gameRoom = DixitRoom{RoomId:rid,gworkRoom:room}
 		}
-		sectionData, _ := cfg.SectionOptions(section)
-		for _, key := range sectionData {
-			value, err := cfg.String(section, key)
-			if err == nil {
-				targetConfig[key] = value
-			}
+		if receiveType == "start" {
+			gameRoom.StartGame(uid)
+		} else if receiveType == "hostpick" {
+			gameRoom.HostPick(receiveNodes["keyword"].(string), receiveNodes["card"].(int))
+		} else if receiveType == "guestpick" {
+			gameRoom.GuestPick(uid, receiveNodes["card"].(int))
+		} else if receiveType == "guess" {
+			gameRoom.Guess(uid, receiveNodes["card"].(int))
 		}
-		break
-	}
-	return targetConfig, nil
+	})
 }
